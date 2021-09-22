124fde63b893127f6faa115b749abdc103db7094
AnneLineC <anneline.col@gmail.com>
2021-09-22 17:16:07 +0200

    structure prete à utilisation

--- app/Models/CoreModel.php
+++ app/Models/CoreModel.php
@@ -3,20 +3,5 @@
 namespace App\Models;
 
 abstract class CoreModel {
-    // 1. Propriétés partagées entre tous les models, en protected (accessibles seulement depuis les classes enfants de CoreModel)
-    // protected $propriété1
-    // protected $propriété2
 
-    // 2. Leurs getters et setters
-
-    // 3. On peut également définir ici des méthodes abstraites :
-    // elles ne peuvent pas être définies dans le CoreModel,
-    // mais elles devront obligatoirement l'être dans ses classes enfant
-    // en respéctant strictement leur structure définie ici (static ou non, nom, paramètres...)
-    // Exemple : 
-    // abstract static public function find($id);
-    // abstract static public function findAll();
-    // abstract public function insert();
-    // abstract public function update();
-    // abstract public function delete();
 }
